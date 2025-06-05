3cf795ceed228f8e21cd98145f7b2300e9d157a6
ta2 <dragon5555555@gmail.com>
2025-06-05 14:19:28 +0900

    Fix: ESLint errors for Vercel build

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import Link from "next/link";
+
 export function Sidebar() {
   return (
     <aside className="w-64 bg-gray-200 p-4">
       <nav className="text-black">
         <ul className="space-y-2">
-          <li><a href="/" className="hover:underline">Home</a></li>
-          <li><a href="/about" className="hover:underline">About</a></li>
-          <li><a href="/contact" className="hover:underline">Contact</a></li>
+          <li><Link href="/" className="hover:underline">Home</Link></li>
+          <li><Link href="/about" className="hover:underline">About</Link></li>
+          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
         </ul>
       </nav>
     </aside>
